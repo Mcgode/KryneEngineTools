5f60f73b7f7046fab3bb939c0732fd33381e34db
maxgodefroy <max@godefroy.net>
2026-03-20 17:02:59 +0100

    [Tools][ProjectManager] Add file event callbacks and verbose logging in `DirectoryMonitor`
    Integrated file create, modify, rename, and delete callbacks into `DirectoryMonitor`. Leveraged `Logger` for detailed verbose logging of file events in the `AssetCooker` log category.

--- ProjectManager/Source/AssetCooker/DirectoryMonitor.cpp
+++ ProjectManager/Source/AssetCooker/DirectoryMonitor.cpp
@@ -6,7 +6,10 @@
 
 #include "DirectoryMonitor.hpp"
 
-#include "KryneEngine/Core/Memory/DynamicArray.hpp"
+#include <KryneEngine/Core/Memory/DynamicArray.hpp>
+
+#include "ProjectManager/AssetCooker/AssetCooker.hpp"
+#include "ProjectManager/Logger/Logger.hpp"
 
 namespace ProjectManager
 {
@@ -18,7 +21,31 @@ namespace ProjectManager
 
         const KryneEngine::Platform::DirectoryMonitorCreateInfo createInfo {
             .m_directories = { directories.Data(), directories.Size() },
-            .m_threadName = "AssetCookerDirectoryMonitor"
+            .m_threadName = "AssetCookerDirectoryMonitor",
+            .m_fileCreatedCallback = [](const eastl::string_view _path)
+            {
+                char msgBuffer[1024];
+                snprintf(msgBuffer, sizeof(msgBuffer), "File created: %s", _path.data());
+                Logger::GetInstance()->Log(LogSeverity::Verbose, AssetCooker::kLogCategory, msgBuffer);
+            },
+            .m_fileModifiedCallback = [](const eastl::string_view _path)
+            {
+                char msgBuffer[1024];
+                snprintf(msgBuffer, sizeof(msgBuffer), "File modified: %s", _path.data());
+                Logger::GetInstance()->Log(LogSeverity::Verbose, AssetCooker::kLogCategory, msgBuffer);
+            },
+            .m_fileRenamedCallback = [](const eastl::string_view _oldPath, const eastl::string_view _newPath)
+            {
+                char msgBuffer[2048];
+                snprintf(msgBuffer, sizeof(msgBuffer), "File renamed: from '%s' to '%s'", _oldPath.data(), _newPath.data());
+                Logger::GetInstance()->Log(LogSeverity::Verbose, AssetCooker::kLogCategory, msgBuffer);
+            },
+            .m_fileDeletedCallback = [](const eastl::string_view _path)
+            {
+                char msgBuffer[1024];
+                snprintf(msgBuffer, sizeof(msgBuffer), "File deleted: %s", _path.data());
+                Logger::GetInstance()->Log(LogSeverity::Verbose, AssetCooker::kLogCategory, msgBuffer);
+            },
         };
 
         m_handle = KryneEngine::Platform::CreateDirectoryMonitor(createInfo, {});
